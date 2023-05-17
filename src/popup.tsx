0df6d771442317a95dd5ac257a3ef7f1204d5de0
Rami Maalouf <rami.maalouf@ibm.com>
2023-05-17 09:36:18 -0400

    popup UI improvements

--- src/popup.tsx
+++ src/popup.tsx
@@ -56,27 +56,31 @@ const Popup = () => {
   };
 
   return (
-    <main className="min-w-max min-h-1/2 p-4">
+    <main className="w-96 min-h-max p-4">
       {inYoutube ? (
         <>
           <h1 className="text-2xl font-bold mb-4 text-center">
             Current Video Bookmarks</h1>
           <ul>
             {currentVideoBookmarks.map((bookmark: Snip, i): JSX.Element => (
-              <li key={i} className="p-4 mb-4">
-                <div className="flex flex-col">
-                  <div className="flex">
+              <li key={i} className="mb-4 border border-gray-300 rounded-md flex flex-row">
+                <img className="h-full" src="https://i.ytimg.com/vi/3qYbVjg5Z7k/hqdefault.jpg" alt="thumbnail" />
+                <div className="flex flex-col justify-start w-full p-4">
+                  {bookmark?.title && (
+                    <div className="font-bold">{bookmark?.title}</div>
+                  )}
+                  <div className="flex flex-row justify-between">
                     <div className="text-gray-500">{Math.floor(bookmark.startTimestamp / 60)}:{String(Math.round(bookmark.startTimestamp) % 60).padStart(2, "0")}</div>
-                    <div className="text-gray-500">{" "}-{" "}</div>
                     <div className="text-gray-500">{Math.floor(bookmark.endTimestamp / 60)}:{String(Math.round(bookmark.endTimestamp) % 60).padStart(2, "0")}</div>
+
                   </div>
-                  <div className="flex">
+                </div>
+                {/* <div className="flex">
                     {bookmark?.tags?.map((tag: Tag, i: number) => (
                       <div key={i} className={`bg-${tag.color ?? "gray"}-300 rounded-full px-2 py-1 text-xs mr-2 border border-${tag.color ?? "gray"}-400`}>{tag.name}</div>
                     ))}
-                  </div>
-                </div>
-                <div className="mt-2">{bookmark?.notes ?? ""}</div>
+                  </div> */}
+                {/* <div className="mt-2">{bookmark?.notes ?? ""}</div> */}
               </li>
             ))}
           </ul>
@@ -86,7 +90,7 @@ const Popup = () => {
         <div className="uppercase text-center text-2xl font-bold whitespace-nowrap" >🚫not in youtube🚫</div>
       )
       }
-    </main >
+    </main>
   );
 };
 {/* <>
