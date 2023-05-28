b6bc33c45977337d0fb9cfdd7bd4b4ef3468c186
Rami Maalouf <rami.maalouf@ibm.com>
2023-05-28 14:17:39 -0400

    feat: you can now call the API to summarize the whole vid or a specific section of it

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+# Backend of SnipTube
+
+## TODO
+
+* [ ] Feat: The snip summary now has context to the whole video instead of just the snip
+* [ ] Feat: You can now add your own API keys to the backend
--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,24 @@ from fastapi import FastAPI
 import requests
 import os
 from youtube_transcript_api import YouTubeTranscriptApi
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import TypedDict
 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import YoutubeLoader
 from langchain.llms import OpenAI
 from langchain.chains.summarize import load_summarize_chain
+from langchain.docstore.document import Document
 
 FLOWISE_API_KEY: str = os.getenv("FLOWISE_API_KEY")
 OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
 API_URL = "http://localhost:3000/api/v1/prediction/08251153-caae-41e7-be83-fd294358e304"
+
+
+class StudentData(TypedDict):
+    text: str
+    start: float
+    duration: float
+
 headers = {"Authorization": "Bearer " + FLOWISE_API_KEY}
 app = FastAPI()
 
@@ -31,19 +40,42 @@ given a youtube video_id, return the video's summary. There are 2 different form
 """
 @app.get("/summary/{video_id}")
 async def summary(video_id: str, start_time: int = None, end_time: int = None):
-    loader = YoutubeLoader.from_youtube_url(f"https://www.youtube.com/watch?v={video_id}", add_video_info=True)
-    transcript = loader.load()
-    
-    # if start_time and end_time: # format 2
-    #     transcript = YouTubeTranscriptApi.get_transcript(video_id, start_time, end_time)
-    # else: # format 1
-    #     transcript = YouTubeTranscriptApi.get_transcript(video_id)
-    
+    # loader = YoutubeLoader.from_youtube_url(f"https://www.youtube.com/watch?v={video_id}", add_video_info=True)
+    # transcript = loader.load()
+        
+    transcript: list[StudentData] = YouTubeTranscriptApi.get_transcript(video_id)
+    if start_time and end_time: # format 2
+        # handle edge cases
+        # if start_time is after the last line, return empty string with error message
+        if start_time > int(transcript[-1]["start"]) + int(transcript[-1]["duration"]) or start_time < 0:
+            return "Error: start_time is out of range"
+        elif end_time < int(transcript[0]["start"]) or end_time > int(transcript[-1]["start"]) + int(transcript[-1]["duration"]):
+            return "Error: end_time is out of range"
+        elif start_time > end_time:
+            return "Error: start_time is after end_time"
+        elif start_time == end_time:
+            return "Error: start_time is equal to end_time"
+            
+        text = ""
+        for line in transcript:
+            from_time = int(line["start"])
+            to_time = from_time + int(line["duration"])
+            # if interval touches the line
+            if (start_time <= from_time and end_time >= from_time) or (start_time <= to_time and end_time >= to_time):
+                text += line["text"].replace("\n", "")
+            elif to_time > end_time: # if interval is after the line
+                break
+    else: # format 1
+        text = [line["text"].replace("\n", "") for line in transcript]
+            
     llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-    chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=True)
+    chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=False)
     
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
 
-    texts = text_splitter.split_documents(transcript)
+    # langchain bug temp fix to make .run() work
+    texts = text_splitter.split_documents([Document(page_content=text, metadata={"video_id": video_id})])
+    print(texts)
 
-    return chain.run(texts)
+    summary = chain.run(texts)
+    return summary
