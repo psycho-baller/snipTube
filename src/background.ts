672efc6f3d5816f3bcc7f998dea8fac7d1d35f66
Rami Maalouf <rami.maalouf@ibm.com>
2023-08-01 23:12:16 -0400

    script is injected so that its always available in yt/watch

--- src/background.ts
+++ src/background.ts
@@ -47,3 +47,15 @@ chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
     });
   }
 });
+
+// https://stackoverflow.com/questions/10994324/chrome-extension-content-script-re-injection-after-upgrade-or-install
+chrome.runtime.onInstalled.addListener(async () => {
+  for (const cs of chrome.runtime.getManifest().content_scripts) {
+    for (const tab of await chrome.tabs.query({ url: cs.matches })) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: cs.js,
+      });
+    }
+  }
+});
