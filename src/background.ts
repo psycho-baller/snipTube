4be87601342ef3484e318f99c443dd1373d88e12
Rami Maalouf <rami.maalouf@ibm.com>
2024-04-27 01:45:23 -0400

    linting and settings page is more clear

--- src/background.ts
+++ src/background.ts
@@ -1,52 +1,55 @@
-import 'webextension-polyfill-global';
+import "webextension-polyfill-global";
 
 browser.runtime.onInstalled.addListener((details) => {
-  // take the user to the demo page after installation in production
-  if (process.env.NODE_ENV === "production" && details.reason === "install") {
-    browser.tabs.create({ url: "https://sniptube.tech#demo" });
-  }
-  // browser.contextMenus.create({
-  //   id: "youtube",
-  //   title: "Add to Snip",
-  //   contexts: ["link"],
-  // });
+	// take the user to the demo page after installation in production
+	if (process.env.NODE_ENV === "production" && details.reason === "install") {
+		browser.tabs.create({ url: "https://sniptube.tech#demo" });
+	}
+	// browser.contextMenus.create({
+	//   id: "youtube",
+	//   title: "Add to Snip",
+	//   contexts: ["link"],
+	// });
 });
 
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.active) {
-    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-    const activeTab = tabs[0];
-    if (activeTab.url && activeTab.url.includes("youtube.com/watch")) {
-      const queryParameters = activeTab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-      const vidId = urlParameters.get("v");
+	if (changeInfo.status === "complete" && tab.active) {
+		const tabs = await browser.tabs.query({
+			active: true,
+			currentWindow: true,
+		});
+		const activeTab = tabs[0];
+		if (activeTab.url?.includes("youtube.com/watch")) {
+			const queryParameters = activeTab.url.split("?")[1];
+			const urlParameters = new URLSearchParams(queryParameters);
+			const vidId = urlParameters.get("v");
 
-      sendMessageToContentScript(tabId, {
-        type: "NEW",
-        vidId,
-      });
-    }
-  }
+			sendMessageToContentScript(tabId, {
+				type: "NEW",
+				vidId,
+			});
+		}
+	}
 });
 
 /**
  * This listener is triggered when the user is switching between tabs.
  */
 browser.tabs.onActivated.addListener(async (activeInfo) => {
-  // Retrieve information about the newly activated tab
-  const activeTab = await browser.tabs.get(activeInfo.tabId);
+	// Retrieve information about the newly activated tab
+	const activeTab = await browser.tabs.get(activeInfo.tabId);
 
-  // Your existing code for checking the URL and sending messages
-  if (activeTab.url && activeTab.url.includes("youtube.com/watch")) {
-    const queryParameters = activeTab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-    const vidId = urlParameters.get("v");
-    console.log("vidId", vidId);
-    sendMessageToContentScript(activeTab.id, {
-      type: "UPDATE_VIDEO_ID",
-      vidId,
-    });
-  }
+	// Your existing code for checking the URL and sending messages
+	if (activeTab.url?.includes("youtube.com/watch")) {
+		const queryParameters = activeTab.url.split("?")[1];
+		const urlParameters = new URLSearchParams(queryParameters);
+		const vidId = urlParameters.get("v");
+		console.log("vidId", vidId);
+		sendMessageToContentScript(activeTab.id, {
+			type: "UPDATE_VIDEO_ID",
+			vidId,
+		});
+	}
 });
 
 // chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
@@ -73,40 +76,45 @@ browser.tabs.onActivated.addListener(async (activeInfo) => {
 // });
 
 browser.runtime.connect().onDisconnect.addListener(async () => {
-  // clean up when content script gets disconnected
-  console.log("content script disconnected");
-  for (const cs of browser.runtime.getManifest().content_scripts) {
-    for (const tab of await browser.tabs.query({ url: cs.matches })) {
-      browser.scripting
-        .executeScript({
-          target: { tabId: tab.id },
-          files: cs.js,
-        })
-        .then(() => {
-          console.log("content script injected");
-        })
-        .catch((err) => {
-          console.log("error injecting content script", err);
-        });
-    }
-  }
+	// clean up when content script gets disconnected
+	console.log("content script disconnected");
+	for (const cs of browser.runtime.getManifest().content_scripts) {
+		for (const tab of await browser.tabs.query({ url: cs.matches })) {
+			browser.scripting
+				.executeScript({
+					target: { tabId: tab.id },
+					files: cs.js,
+				})
+				.then(() => {
+					console.log("content script injected");
+				})
+				.catch((err) => {
+					console.log("error injecting content script", err);
+				});
+		}
+	}
 });
 
 const maxRetryAttempts = 3;
 
-async function sendMessageToContentScript(tabId: number, message: any, retryCount = 0) {
-  try {
-    browser.tabs.sendMessage(tabId, message).then((response) => {
-      console.log("response", response);
-    });
-  } catch (error) {
-    if (retryCount < maxRetryAttempts) {
-      setTimeout(() => {
-        sendMessageToContentScript(tabId, message, retryCount + 1);
-      }, 1000); // Retry after 1 second
-    } else {
-      console.error("Failed to send message after multiple attempts", error);
-    }
-  }
+async function sendMessageToContentScript(
+	tabId: number,
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	message: any,
+	retryCount = 0,
+) {
+	try {
+		browser.tabs.sendMessage(tabId, message).then((response) => {
+			console.log("response", response);
+		});
+	} catch (error) {
+		if (retryCount < maxRetryAttempts) {
+			setTimeout(() => {
+				sendMessageToContentScript(tabId, message, retryCount + 1);
+			}, 1000); // Retry after 1 second
+		} else {
+			console.error("Failed to send message after multiple attempts", error);
+		}
+	}
 }
 export {};
--- src/popup/components/SettingsForm.tsx
+++ src/popup/components/SettingsForm.tsx
@@ -1,108 +1,122 @@
 // TODO: add a button for the users to add their own button
 import { useState, type FC, useEffect, type ChangeEvent, useMemo } from "react";
 import {
-  deleteAllDataFromStorage,
-  getDefaultSnipLength,
-  getPauseVideoOnNewSnip,
-  getShowOverlayOnNewSnip,
-  getUseKeyboardShortcut,
-  setDefaultSnipLength,
-  setPauseVideoOnNewSnip,
-  setShowAddSnipDetailsFormOnNewSnip,
-  setUseKeyboardShortcut,
+	deleteAllDataFromStorage,
+	getDefaultSnipLength,
+	getPauseVideoOnNewSnip,
+	getShowOverlayOnNewSnip,
+	getUseKeyboardShortcut,
+	setDefaultSnipLength,
+	setPauseVideoOnNewSnip,
+	setShowAddSnipDetailsFormOnNewSnip,
+	setUseKeyboardShortcut,
 } from "~lib/storage";
 import { useAllSnipsStore, useSnipsStore } from "~stores/sniptube";
 interface Props {
-  className?: string;
-  takeUpFullHeight?: boolean;
+	className?: string;
+	takeUpFullHeight?: boolean;
 }
 
 const SettingsForm: FC<Props> = (props) => {
-  const { className, takeUpFullHeight } = props;
+	const { className, takeUpFullHeight } = props;
 
-  const setAllSnips = useAllSnipsStore((state) => state.setSnips);
-  const setSnips = useSnipsStore((state) => state.setSnips);
+	const setAllSnips = useAllSnipsStore((state) => state.setSnips);
+	const setSnips = useSnipsStore((state) => state.setSnips);
 
-  // get viewport height
+	// get viewport height
 
-  const [showAddSnipDetailsFormOnNewSnipState, setShowAddSnipDetailsFormOnNewSnipState] = useState<boolean>(true);
-  const [pauseVideoOnNewSnipState, setPauseVideoOnNewSnipState] = useState<boolean>(true);
-  const [useKeyboardShortcutState, setUseKeyboardShortcutState] = useState<boolean>(true);
-  const [length, setLength] = useState<number>(30);
+	const [
+		showAddSnipDetailsFormOnNewSnipState,
+		setShowAddSnipDetailsFormOnNewSnipState,
+	] = useState<boolean>(true);
+	const [pauseVideoOnNewSnipState, setPauseVideoOnNewSnipState] =
+		useState<boolean>(true);
+	const [useKeyboardShortcutState, setUseKeyboardShortcutState] =
+		useState<boolean>(true);
+	const [length, setLength] = useState<number>(30);
 
-  const handleSave = async () => {
-    console.log("autosave", length);
-    await setDefaultSnipLength(length);
-  };
+	const handleSave = async () => {
+		console.log("autosave", length);
+		await setDefaultSnipLength(length);
+	};
 
-  const handleLengthChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    setLength(parseInt(e.target.value));
-    // await setDefaultSnipLength(parseInt(e.target.value));
-  };
+	const handleLengthChange = async (e: ChangeEvent<HTMLInputElement>) => {
+		setLength(Number.parseInt(e.target.value));
+		// await setDefaultSnipLength(parseInt(e.target.value));
+	};
 
-  const handleShowOverlayOnNewSnipChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    setShowAddSnipDetailsFormOnNewSnipState(e.target.checked);
-    await setShowAddSnipDetailsFormOnNewSnip(e.target.checked);
-  };
+	const handleShowOverlayOnNewSnipChange = async (
+		e: ChangeEvent<HTMLInputElement>,
+	) => {
+		setShowAddSnipDetailsFormOnNewSnipState(e.target.checked);
+		await setShowAddSnipDetailsFormOnNewSnip(e.target.checked);
+	};
 
-  const handlePauseVideoOnNewSnipChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    setPauseVideoOnNewSnipState(e.target.checked);
-    await setPauseVideoOnNewSnip(e.target.checked);
-  };
+	const handlePauseVideoOnNewSnipChange = async (
+		e: ChangeEvent<HTMLInputElement>,
+	) => {
+		setPauseVideoOnNewSnipState(e.target.checked);
+		await setPauseVideoOnNewSnip(e.target.checked);
+	};
 
-  const handleUseKeyboardShortcutChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    setUseKeyboardShortcutState(e.target.checked);
-    await setUseKeyboardShortcut(e.target.checked);
-  };
+	const handleUseKeyboardShortcutChange = async (
+		e: ChangeEvent<HTMLInputElement>,
+	) => {
+		setUseKeyboardShortcutState(e.target.checked);
+		await setUseKeyboardShortcut(e.target.checked);
+	};
 
-  useEffect(() => {
-    new Promise<number>((resolve) => {
-      getDefaultSnipLength().then((length) => {
-        console.log("length", length);
-        setLength(length);
-        resolve(length);
-      });
-    });
+	useEffect(() => {
+		new Promise<number>((resolve) => {
+			getDefaultSnipLength().then((length) => {
+				console.log("length", length);
+				setLength(length);
+				resolve(length);
+			});
+		});
 
-    new Promise<boolean>((resolve) => {
-      getShowOverlayOnNewSnip().then((show) => {
-        setShowAddSnipDetailsFormOnNewSnipState(show);
-        resolve(show);
-      });
-    });
+		new Promise<boolean>((resolve) => {
+			getShowOverlayOnNewSnip().then((show) => {
+				setShowAddSnipDetailsFormOnNewSnipState(show);
+				resolve(show);
+			});
+		});
 
-    new Promise<boolean>((resolve) => {
-      getPauseVideoOnNewSnip().then((pause) => {
-        setPauseVideoOnNewSnipState(pause);
-        resolve(pause);
-      });
-    });
+		new Promise<boolean>((resolve) => {
+			getPauseVideoOnNewSnip().then((pause) => {
+				setPauseVideoOnNewSnipState(pause);
+				resolve(pause);
+			});
+		});
 
-    new Promise<boolean>((resolve) => {
-      getUseKeyboardShortcut().then((use) => {
-        setUseKeyboardShortcutState(use);
-        resolve(use);
-      });
-    });
-  }, []);
+		new Promise<boolean>((resolve) => {
+			getUseKeyboardShortcut().then((use) => {
+				setUseKeyboardShortcutState(use);
+				resolve(use);
+			});
+		});
+	}, []);
 
-  async function deleteAllData() {
-    await deleteAllDataFromStorage();
-    setAllSnips([]);
-    setSnips([]);
-  }
+	async function deleteAllData() {
+		await deleteAllDataFromStorage();
+		setAllSnips([]);
+		setSnips([]);
+	}
 
-  return (
-    <section className={className}>
-      <div
-        className={
-          "overflow-scroll flex flex-col gap-y-6 px-4 pt-2 pb-4 " + (takeUpFullHeight ? "" : "max-h-[22.5rem]")
-        }
-      >
-        <div className="flex items-center justify-between">
-          {/* keep in single line:  overflow-ellipsis whitespace-nowrap  */}
-          <h2 className="-mb-2 overflow-hidden text-2xl font-medium">Settings</h2>
-          {/* <button
+	return (
+		<section className={className}>
+			<div
+				className={
+					"overflow-scroll flex flex-col gap-y-6 px-4 pt-2 pb-4 " +
+					(takeUpFullHeight ? "" : "max-h-[22.5rem]")
+				}
+			>
+				<div className="flex items-center justify-between">
+					{/* keep in single line:  overflow-ellipsis whitespace-nowrap  */}
+					<h2 className="-mb-2 overflow-hidden text-2xl font-medium">
+						Settings
+					</h2>
+					{/* <button
             type="button"
             className="text-gray-500 bg-transparent hover:text-gray-700"
             aria-label="Add Note After Saving Snip Info"
@@ -116,119 +130,117 @@ const SettingsForm: FC<Props> = (props) => {
               ?
             </svg>
           </button> */}
-        </div>
-        <div className="">
-          <label
-            htmlFor="useKeyboardShortcut"
-            className="text-base"
-          >
-            Create snip with the 's' key
-          </label>
-          <div className="flex items-center mt-2">
-            <input
-              id="useKeyboardShortcut"
-              type="checkbox"
-              checked={useKeyboardShortcutState}
-              className="w-4 h-4 text-gray-500 bg-gray-700 border-gray-700 rounded form-checkbox focus:ring-gray-500 focus:ring-offset-gray-800 focus:outline-none"
-              onChange={handleUseKeyboardShortcutChange}
-            />
-            {/* <label
+				</div>
+				<div className="">
+					<label htmlFor="useKeyboardShortcut" className="text-base">
+						Create snip with the 's' key
+					</label>
+					<div className="flex items-center mt-2">
+						<input
+							id="useKeyboardShortcut"
+							type="checkbox"
+							checked={useKeyboardShortcutState}
+							className="w-4 h-4 text-gray-500 bg-gray-700 border-gray-700 rounded form-checkbox focus:ring-gray-500 focus:ring-offset-gray-800 focus:outline-none"
+							onChange={handleUseKeyboardShortcutChange}
+						/>
+						{/* <label
               htmlFor="addNoteAfterSaving"
               className="ml-2"
             >
               Enable
             </label> */}
-          </div>
-        </div>
-        <div className="">
-          <label
-            htmlFor="addNoteAfterSaving"
-            className="text-base"
-          >
-            Add note after creating snip
-          </label>
-          <div className="flex items-center mt-2">
-            <input
-              id="addNoteAfterSaving"
-              type="checkbox"
-              checked={showAddSnipDetailsFormOnNewSnipState}
-              className="w-4 h-4 text-gray-500 bg-gray-700 border-gray-700 rounded form-checkbox focus:ring-gray-500 focus:ring-offset-gray-800 focus:outline-none"
-              onChange={handleShowOverlayOnNewSnipChange}
-            />
-            {/* <label
+					</div>
+				</div>
+				<div className="">
+					<label htmlFor="addNoteAfterSaving" className="text-base">
+						Add note when creating a snip
+					</label>
+					<div className="text-xs text-gray-400">
+						Enabling this will show a popup when you create a snip
+					</div>
+					<div className="flex items-center mt-2">
+						<input
+							id="addNoteAfterSaving"
+							type="checkbox"
+							checked={showAddSnipDetailsFormOnNewSnipState}
+							className="w-4 h-4 text-gray-500 bg-gray-700 border-gray-700 rounded form-checkbox focus:ring-gray-500 focus:ring-offset-gray-800 focus:outline-none"
+							onChange={handleShowOverlayOnNewSnipChange}
+						/>
+						{/* <label
               htmlFor="addNoteAfterSaving"
               className="ml-2"
             >
               Enable
             </label> */}
-          </div>
-        </div>
-        <div className={"" + (showAddSnipDetailsFormOnNewSnipState ? " " : " hidden")}>
-          <label
-            htmlFor="showAddSnipDetailsFormOnNewSnip"
-            className="text-base"
-          >
-            Pause video when creating snip
-          </label>
-          <div className="flex items-center mt-2">
-            <input
-              id="showAddSnipDetailsFormOnNewSnip"
-              checked={pauseVideoOnNewSnipState}
-              type="checkbox"
-              className="w-4 h-4 text-gray-500 bg-gray-700 border-gray-700 rounded form-checkbox focus:ring-gray-500 focus:ring-offset-gray-800 focus:outline-none"
-              onChange={handlePauseVideoOnNewSnipChange}
-            />
-            {/* <label
+					</div>
+				</div>
+				<div
+					className={
+						"" + (showAddSnipDetailsFormOnNewSnipState ? " " : " hidden")
+					}
+				>
+					<label
+						htmlFor="showAddSnipDetailsFormOnNewSnip"
+						className="text-base"
+					>
+						Pause video when creating snip
+					</label>
+					<div className="flex items-center mt-2">
+						<input
+							id="showAddSnipDetailsFormOnNewSnip"
+							checked={pauseVideoOnNewSnipState}
+							type="checkbox"
+							className="w-4 h-4 text-gray-500 bg-gray-700 border-gray-700 rounded form-checkbox focus:ring-gray-500 focus:ring-offset-gray-800 focus:outline-none"
+							onChange={handlePauseVideoOnNewSnipChange}
+						/>
+						{/* <label
               htmlFor="showAddSnipDetailsFormOnNewSnip"
               className="ml-2"
             >
               Enable
             </label> */}
-          </div>
-        </div>
-        <div className="">
-          <label
-            htmlFor="defaultSnipLength"
-            className="text-base"
-          >
-            Default Snip Length
-          </label>
-          <div className="flex items-center mt-2 !ring-gray-600 hover:ring-2 rounded-lg">
-            <input
-              id="defaultSnipLength"
-              type="number"
-              className="w-16 form-input border-none !ring-0 rounded-l-lg bg-inherit focus:outline-none"
-              value={length}
-              onChange={handleLengthChange}
-              onBlur={handleSave}
-              min={20}
-              max={120}
-            />
-            <input
-              type="range"
-              className="w-full mr-3 focus:outline-none"
-              value={length}
-              onChange={handleLengthChange}
-              onBlur={handleSave}
-              min={20}
-              max={120}
-            />
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <h3 className="text-lg font-medium pb-1">Danger Zone</h3>
-          <button
-            type="button"
-            className="w-full px-4 py-2 mt-2 text-sm font-medium text-red-500 bg-transparent border border-red-500 rounded-md hover:bg-red-500 hover:text-white transition-all duration-200 ease-in-out"
-            aria-label="Erase all data"
-            onClick={deleteAllData}
-          >
-            Reset all your app data
-          </button>
-        </div>
-      </div>
-    </section>
-  );
+					</div>
+				</div>
+				<div className="">
+					<label htmlFor="defaultSnipLength" className="text-base">
+						Default Snip Length
+					</label>
+					<div className="flex items-center mt-2 !ring-gray-600 hover:ring-2 rounded-lg">
+						<input
+							id="defaultSnipLength"
+							type="number"
+							className="w-16 form-input border-none !ring-0 rounded-l-lg bg-inherit focus:outline-none"
+							value={length}
+							onChange={handleLengthChange}
+							onBlur={handleSave}
+							min={20}
+							max={120}
+						/>
+						<input
+							type="range"
+							className="w-full mr-3 focus:outline-none"
+							value={length}
+							onChange={handleLengthChange}
+							onBlur={handleSave}
+							min={20}
+							max={120}
+						/>
+					</div>
+				</div>
+				<div className="flex flex-col">
+					<h3 className="text-lg font-medium pb-1">Danger Zone</h3>
+					<button
+						type="button"
+						className="w-full px-4 py-2 mt-2 text-sm font-medium text-red-500 bg-transparent border border-red-500 rounded-md hover:bg-red-500 hover:text-white transition-all duration-200 ease-in-out"
+						aria-label="Erase all data"
+						onClick={deleteAllData}
+					>
+						Reset all your app data
+					</button>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default SettingsForm;
